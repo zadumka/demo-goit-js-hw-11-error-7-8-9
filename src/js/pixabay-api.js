291e6d9b2e43d5383f037cf19ad40750874b72e7
zadumka <90329284+zadumka@users.noreply.github.com>
2025-06-04 12:57:48 +0300

    Update pixabay-api.js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,27 +1,29 @@
 import axios from 'axios';
 
-const API_KEY = '49351008-1bfee9cf32a9c846c40651839';
+const API_KEY = '50377156-76a0d970257c0a39042cd42de';
 const BASE_URL = 'https://pixabay.com/api/';
 
-export function fetchImages(query) {
-  const params = {
-    key: API_KEY,
-    q: query,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    per_page: 20,
-    safesearch: true,
-  };
+// delay (for testing only)
+function delay(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
-  return axios
-    .get(BASE_URL, { params })
-    .then(response => {
-      if (!response.data.hits.length) {
-        return Promise.reject(new Error('No images found'));
-      }
-      return response.data.hits;
-    })
-    .catch(error => {
-      return Promise.reject(error);
+export async function getImagesByQuery(query) {
+  try {
+    await delay(2000); //
+
+    const response = await axios.get(`${BASE_URL}`, {
+      params: {
+        key: API_KEY,
+        q: query,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+      },
     });
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching images:', error);
+    throw error;
+  }
 }
